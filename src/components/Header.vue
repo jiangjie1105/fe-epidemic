551131821fa6ca2c47238161814f7b5daec7f5b4
jiangjie1105 <1021404964@qq.com>
2020-09-18 12:53:08 +0800

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
-    header
+    <div class="container clearfix">header</div>
+    <div class="pv">
+      <i class="ll">xxx人已浏览</i>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header',
+  name: "Header",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <style scoped>
-
+.container {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 33.5%;
+  color: #fff;
+  font-size: 0.28rem;
+  text-align: center;
+  background: url(https://img1.dxycdn.com/2020/0314/863/3401956761707181858-2.png)
+    no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.pv {
+  position: absolute;
+  right: 0;
+  top: 10px;
+  color: #fff;
+  font-size: 12px;
+  height: 24px;
+  line-height: 24px;
+  border-top-left-radius: 24px;
+  border-bottom-left-radius: 24px;
+  background-color: rgba(19, 15, 133, 0.6);
+}
+.pv .ll {
+  padding: 0 10px;
+}
 </style>
